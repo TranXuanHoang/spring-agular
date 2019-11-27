c33bf695e49ff3e3a6fa5a0cb042d23718ee5e93
TranXuanHoang <hoangtx.ict@gmail.com>
2019-11-28 01:00:08 +0900

    initial commit

---
+++ src/test/java/spring/angular/VersionTestChild3ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionTestChild3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
