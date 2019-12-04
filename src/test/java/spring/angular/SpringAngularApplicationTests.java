c485a1740293a99b681e2f98011a295e9bd967f8
TranXuanHoang <hoangtx.ict@gmail.com>
2019-12-04 16:40:06 +0900

    Secure web app with Spring security's basic authorization

--- src/test/java/spring/angular/SpringAngularApplicationTests.java
+++ src/test/java/spring/angular/SpringAngularApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VersionTestChild3ApplicationTests {
+class SpringAngularApplicationTests {
 
 	@Test
 	void contextLoads() {
